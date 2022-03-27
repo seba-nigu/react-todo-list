c1d275362ad25b58914d5b4d4c371d2633229f36
seba-nigu <seba80443@gmail.com>
2022-03-27 19:42:51 +0300

    added task box component

---
+++ src/Components/TaskBox/TaskBox.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function TaskBox(props) {
+  return (
+    <div
+      className="TaskBox"
+      style={{
+        color: props.theme.white,
+        backgroundColor: props.theme.normalText,
+      }}
+    >
+      <div className="taskbox-text">{props.text}</div>
+      <div className="textbox-category">{props.category}</div>
+    </div>
+  );
+}
+
+export default TaskBox;
--- src/Pages/MyTasks/MyTasks.js
+++ src/Pages/MyTasks/MyTasks.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Dropdown from "../../Components/Dropdown/Dropdown";
 import SearchBar from "../../Components/SearchBar/SearchBar";
+import TaskBox from "../../Components/TaskBox/TaskBox";
 import "./style.css";
 function MyTaks(props) {
   return (
     <div className="MyTasks">
-      MyTaks
-      <SearchBar placeholder={"Search..."} width="200px" theme={props.theme} />
-      <Dropdown theme={props.theme} />
+      <div
+        className="mytasks-text"
+        style={{
+          color: props.theme.normalText,
+        }}
+      >
+        MyTaks
+      </div>
+      <div className="search-area">
+        <SearchBar
+          placeholder={"Search..."}
+          width="250px"
+          theme={props.theme}
+        />
+        <Dropdown theme={props.theme} />
+      </div>
+      <div className="task-boxes">
+        <TaskBox
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam"
+          category="Work"
+          theme={props.theme}
+        />
+      </div>
     </div>
   );
 }
