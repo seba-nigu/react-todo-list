9e4b6d06f463617ee6127beb3a95e979ad273bef
seba-nigu <seba80443@gmail.com>
2022-04-07 14:13:40 +0300

    solved search, but only works if you type the button twice

--- frontend/src/Pages/MyTasks/MyTasks.js
+++ frontend/src/Pages/MyTasks/MyTasks.js
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from "react";
-import Dropdown from "../../Components/Dropdown/Dropdown";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import TaskBox from "../../Components/TaskBox/TaskBox";
+import MainButton from "../../Components/MainButton/MainButton";
+
 import "./style.css";
 import axios from "axios";
 
 function MyTaks(props) {
   const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  function searchTask() {
+    setInput(
+      document.querySelector(".search-area").children[0].children[0].value
+    );
+    let element = document.querySelector(".task-boxes");
+    let children = element.children;
+    for (let i = 0; i < children.length; i++) {
+      let childText = children[i].children[0].innerText;
+      if (input != "") {
+        if (childText !== input) {
+          children[i].style.display = "none";
+        } else {
+          children[i].style.display = "block";
+        }
+      } else {
+        children[i].style.display = "block";
+      }
+    }
+  }
+
   function getTask() {
     axios
       .get("https://localhost:44351/tasks")
@@ -21,6 +44,7 @@ function MyTaks(props) {
   useEffect(() => {
     getTask();
   }, []);
+
   return (
     <div className="MyTasks">
       <div
@@ -37,13 +61,22 @@ function MyTaks(props) {
           width="250px"
           theme={props.theme}
         />
-        <Dropdown theme={props.theme} datalistName={"categories"} />
+        <MainButton
+          backgroundColor={props.theme.mainButton}
+          text={"Search"}
+          handleClick={searchTask}
+        />
       </div>
       <div className="task-boxes">
         {
           <>
             {tasks.map((task) => (
-              <TaskBox text={task.name} category="Work" theme={props.theme} />
+              <TaskBox
+                text={task.name}
+                category="Work"
+                theme={props.theme}
+                key={task.id}
+              />
             ))}
           </>
         }
