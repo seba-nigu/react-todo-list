472497432b7f659109926136752cdbe9b3b0130e
seba-nigu <102463065+seba-nigu@users.noreply.github.com>
2022-04-08 10:24:32 +0300

    changed category to use axios get

--- frontend/src/Components/TaskBox/TaskBox.js
+++ frontend/src/Components/TaskBox/TaskBox.js
@@ -1,7 +1,23 @@
-import React from "react";
 import "./style.css";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function TaskBox(props) {
+  const [category, setCategory] = useState("");
+  useEffect(() => {
+    getCategoryName(props.test);
+  }, []);
+
+  function getCategoryName(categoryId) {
+    axios
+      .get(`https://localhost:44351/categories/${categoryId}`, categoryId)
+      .then(function (response) {
+        setCategory(response.data.name);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <div
       className="TaskBox"
@@ -11,7 +27,7 @@ function TaskBox(props) {
       }}
     >
       <div className="taskbox-text">{props.text}</div>
-      <div className="taskbox-category">{props.category}</div>
+      <div className="taskbox-category">{category}</div>
     </div>
   );
 }
